a0e57b382abe3d4caed9d2371672fd003bed4c61
kimzhang <analyst004@gmail.com>
2012-10-11 00:55:30 +0800

    增加了nmake的release

---
+++ build/x86/nmake/release/include/autoit.h
@@ -0,0 +1,23 @@
+﻿#ifndef LIB_AUTOIT_H_
+#define LIB_AUTOIT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//查找Autoit 数据 
+const char* au_open_script(
+    const char* stream, 
+    size_t stream_size);
+
+//dump脚本
+bool au_dump_script( 
+	const char *logfile, 
+    const char *stream, 
+    size_t stream_size);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
